b2287e2ac38702812e4502d99d20e2a162c4ef9f
DanPlew <dpplewinski@gmail.com>
2021-06-22 23:56:21 +0200

    ..

--- src/main/java/ufp/uczelnianeforumproblemowe/security/SecurityConfig.java
+++ src/main/java/ufp/uczelnianeforumproblemowe/security/SecurityConfig.java
@@ -8,10 +8,13 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import ufp.uczelnianeforumproblemowe.jpa.enums.RangaEnum;
 import ufp.uczelnianeforumproblemowe.security.uzytkownikDetails.UzytkownikDetailsService;
+
+import javax.servlet.http.HttpServletResponse;
 
 @Configuration
 @EnableWebSecurity
@@ -58,6 +61,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout().permitAll()
                 .and()
+                .cors().and()
                 .csrf().disable();
     }
 }
